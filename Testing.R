ceafd08afa15061266d6be39c09a3a1464f39b2a
MicahRietschlin <micah_1@hotmail.com>
2020-10-03 05:15:18 -0400

    Looking to finish this course

---
+++ Testing.R
@@ -0,0 +1,7 @@
+print ("This file was created within RStudio")
+
+print ("And now it lives on GitHub")
+
+print (" Make a change")
+
+print ("I want to learn and make using both R and version controls a normal practice")
